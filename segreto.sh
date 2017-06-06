a2f0c6bdeeeaf95dc8b966cbf90553ecb5e2d3b0
Segreto-Team <lkdxql@hi2.in>
2017-06-06 21:44:18 +0430

    SegretoTeam

---
+++ segreto.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+cd $HOME/Segreto
+
+install() {
+	    cd libs
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		wget https://valtman.name/files/telegram-cli-1222
+		mv telegram-cli-1222 tgcli
+		chmod +x tgcli
+		cd ..
+		chmod +x bot
+}
+
+
+if [ "$1" = "install" ]; then
+	print_logo
+	beyondteam
+	logo_play
+	install
+elif [ "$1" = "update" ]; then
+	logo_play
+	beyondteam
+	update
+else
+if [ ! -f ./libs/tgcli ]; then
+    echo "tgcli not found"
+    echo "Run $0 install"
+    exit 1
+fi
+	figlet SEGRETO
+	#sudo service redis-server restart
+	./libs/tgcli -s ./bot/bot.lua $@
+fi
